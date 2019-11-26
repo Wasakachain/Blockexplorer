f545c47d8fa5231195c2035534fddef004446234
Carlos Alfonzo <carlosalfonzo098@gmail.com>
2019-11-26 14:52:43 -0400

    block transactions view bug killed

--- src/components/BlockTransactionsView.jsx
+++ src/components/BlockTransactionsView.jsx
@@ -13,19 +13,19 @@ export default class BlockTransactionsView extends React.Component {
 
   validateAndSearchBlockIndex() {
     const { match: { params: { blockIndex } }, blocks, getBlockByIndex } = this.props;
-    if (!blocks[blockIndex]) {
+    if (!blocks.find(({ index }) => index == blockIndex)) {
       getBlockByIndex(blockIndex);
     }
   }
 
   render() {
-    const { match: { params: { blockIndex } }, loading, message } = this.props;
+    const { match: { params: { blockIndex } }, loading, message, blocks } = this.props;
     return (
       <div className='block-view-wrapper max-width full-width flex wrap'>
         <h1 className='main-block-title full-width main-color'>Transactions</h1>
         <h1 className='secundary-title full-width five-color'>{parseInt(blockIndex) !== 0 ? `For Block #${blockIndex}` : 'For Genesis Block'}</h1>
         {
-          loading ? <Loader /> : (
+          loading || !blocks.find(({ index }) => index == blockIndex) ? <Loader /> : (
             <div className='block-info-panel-container full-width'>
               {
                 message ? (
--- src/containers/TransactionsListContainer.jsx
+++ src/containers/TransactionsListContainer.jsx
@@ -5,6 +5,12 @@ export default connect(
   (state, { reducer, reducerKey, identificator }) => {
     if (reducer) {
       if (identificator !== undefined) {
+        if (reducer === 'blocksReducer' && reducerKey === 'blocksList') {
+          return {
+            data: state[reducer][reducerKey].find(({ index }) => index == identificator),
+            responsive: state.responsiveReducer.responsive
+          }
+        }
         return {
           data: state[reducer][reducerKey][identificator],
           responsive: state.responsiveReducer.responsive
